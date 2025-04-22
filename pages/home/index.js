74ddfe9d3b51d5302a482944f0931c0f3a7f8aa5
Maksim Dubovik <max200289453@gmail.com>
2025-04-22 22:40:06 +0300

    fix: adaptive ui and add burger menu

--- pages/home/index.js
+++ pages/home/index.js
@@ -120,3 +120,25 @@ function updateTimer() {
 // Запуск таймера
 setInterval(updateTimer, 1000);
 updateTimer(); // Первоначальный вызов
+// Бургер меню
+const burgerBtn = document.querySelector('.burger-btn');
+const nav = document.querySelector('.nav');
+burgerBtn.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  burgerBtn.classList.toggle('active');
+})
+// Закрытие при клике вне меню
+document.addEventListener('click', (e) => {
+  if (!nav.contains(e.target) && !burgerBtn.contains(e.target)) {
+    nav.classList.remove('active');
+    burgerBtn.classList.remove('active');
+  }
+});
+
+// Закрытие при клике на ссылку
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('active');
+    burgerBtn.classList.remove('active');
+  });
+});
